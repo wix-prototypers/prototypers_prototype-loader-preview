ab667b9f6f24ff1a2279eedc4ed22e73915a5898
olin ariel <arielo@wix.com>
2022-06-02 21:03:30 +0300

    iframe

--- loader.js
+++ loader.js
@@ -1,13 +1,17 @@
-const onPrototypeReady = () => {
+const onPrototypeReady = (src) => {
   const body = document.querySelector("body");
-  body.classList.add("loading");
-  body.insertAdjacentHTML("afterbegin", ` <span class="loader"></span>`);
 
-  window.addEventListener("load", function (event) {
+  if (!src) {
+    body.classList.add("loading");
+    body.insertAdjacentHTML("afterbegin", `<span class="loader"></span>`);
+  } else {
+    body.insertAdjacentHTML("afterbegin", `<iframe src=${src}></iframe>`);
+  }
+  window.addEventListener("load", function () {
     prototypeFunc();
     body.classList.remove("loading");
     document.querySelector(".loader")?.remove();
     body.classList.add("loaded");
   });
 };
-onPrototypeReady();
+onPrototypeReady("https://www.google.com");
