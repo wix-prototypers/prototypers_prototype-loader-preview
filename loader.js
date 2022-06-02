99ec1883976774d737e7bb81266c77d8b30c577c
olin ariel <arielo@wix.com>
2022-06-03 01:36:01 +0300

    css

--- loader.js
+++ loader.js
@@ -1,17 +1,20 @@
 const prototypeLoader = (loader, delay = 2500) => {
   const body = document.querySelector("body");
-  body.classList.add("loading");
+
   if (loader === "editor-loader") {
+    body.classList.add("frame-loading");
     body.insertAdjacentHTML(
       "afterbegin",
       `  <iframe class="loader-frame" src="https://wix-prototypers.github.io/prototypers_prototype-loader-preview/editor-loader.html" style="height: 102vh; top: -3px;"></iframe>`
     );
   } else if (loader === "editorx-loader") {
+    body.classList.add("frame-loading");
     body.insertAdjacentHTML(
       "afterbegin",
       `  <iframe class="loader-frame" src="https://wix-prototypers.github.io/prototypers_prototype-loader-preview//editorx-loader.html" style="height: 102vh; top: -3px;"></iframe>`
     );
   } else {
+    body.classList.add("loading");
     body.insertAdjacentHTML("afterbegin", `<span class="loader"></span>`);
   }
   window.addEventListener("load", function () {
